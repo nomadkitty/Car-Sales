b2a1df00566d53405ce5ef514e5bece799e1b689
Jojo Zhang <jojozhang0327@gmail.com>
2019-09-11 19:01:14 -0400

    complete MVP

--- src/App.js
+++ src/App.js
@@ -1,48 +1,43 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
+// actions
+import { removeFeature, buyItem } from './actions'
 
 import Header from './components/Header';
 import AddedFeatures from './components/AddedFeatures';
 import AdditionalFeatures from './components/AdditionalFeatures';
 import Total from './components/Total';
 
-const App = () => {
-  const state = {
-    additionalPrice: 0,
-    car: {
-      price: 26395,
-      name: '2019 Ford Mustang',
-      image:
-        'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
-      features: []
-    },
-    store: [
-      { id: 1, name: 'V-6 engine', price: 1500 },
-      { id: 2, name: 'Racing detail package', price: 1500 },
-      { id: 3, name: 'Premium sound system', price: 500 },
-      { id: 4, name: 'Rear spoiler', price: 250 }
-    ]
-  };
-
-  const removeFeature = item => {
-    // dispatch an action here to remove an item
-  };
-
-  const buyItem = item => {
-    // dipsatch an action here to add an item
-  };
+const App = (props) => {
+  console.log(props);
+  const { state, removeFeature, buyItem } = props
 
   return (
     <div className="boxes">
       <div className="box">
         <Header car={state.car} />
-        <AddedFeatures car={state.car} />
+        <AddedFeatures 
+        car={state.car}
+        removeFeature={removeFeature}
+        />
       </div>
       <div className="box">
-        <AdditionalFeatures store={state.store} />
+        <AdditionalFeatures 
+        store={state.store} 
+        buyItem={buyItem}
+        />
         <Total car={state.car} additionalPrice={state.additionalPrice} />
       </div>
     </div>
   );
 };
 
-export default App;
+const mapStateToProps = state => {
+  console.log('hi', state);
+  return {
+    state
+  }
+}
+
+export default connect(mapStateToProps, { removeFeature, buyItem })(App);
---
+++ src/reducers/carReducer.js
@@ -0,0 +1,46 @@
+import { BUY_ITEM, REMOVE_FEATURE } from '../actions'
+
+const initialState = {
+    additionalPrice: 0,
+    car: {
+      price: 26395,
+      name: '2019 Ford Mustang',
+      image:
+        'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
+      features: []
+    },
+    store: [
+      { id: 1, name: 'V-6 engine', price: 1500 },
+      { id: 2, name: 'Racing detail package', price: 1500 },
+      { id: 3, name: 'Premium sound system', price: 500 },
+      { id: 4, name: 'Rear spoiler', price: 250 }
+    ]
+}
+
+const carReducer = (state=initialState, action) => {
+  switch (action.type) {
+    case BUY_ITEM: 
+      return {
+        ...state,
+        car: {
+          ...state.car,
+          price: state.car.price + action.payload.price, 
+          features: [...state.car.features, action.payload]
+        }
+      }
+    case REMOVE_FEATURE:
+      return {
+        ...state,
+        car: {
+          ...state.car,
+          price: state.car.price - action.payload.price,
+          features: state.car.features.filter(item => 
+            item.id ===action.payload.id ? !item : item)
+        }
+      }
+    default:
+      return state
+  }
+}
+
+export default carReducer
